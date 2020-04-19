9376bf9c9cc17e8cda36645c331898920f7d07a2
Allennicion <zhangwenj19970616@Gmail.com>
2020-04-19 21:27:56 +0800

    2020-04-19

--- src/main/java/com/cy/blog/web/BiliBiliController.java
+++ src/main/java/com/cy/blog/web/BiliBiliController.java
@@ -1,7 +1,10 @@
 package com.cy.blog.web;
 
+import com.cy.blog.vo.AjaxJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
 
 /**
  * User: 张文杰
@@ -14,5 +17,12 @@ public class BiliBiliController {
     @RequestMapping("/bilibili")
     public String bilibili() {
         return "bilibili";
+    }
+
+    @RequestMapping("/bilibili/file")
+    public AjaxJson imgUp(File file) {
+        AjaxJson aj = new AjaxJson();
+        System.out.println(file.toString());
+        return aj;
     }
 }
