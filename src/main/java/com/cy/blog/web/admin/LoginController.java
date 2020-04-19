0b779943d510e72e4421a2eeececc4cfcbf3fdec
Allennicion <Allennicion@users.noreply.github.com>
2020-04-19 09:39:08 +0800

    blog -v1.0

--- src/main/java/com/cy/blog/web/admin/LoginController.java
+++ src/main/java/com/cy/blog/web/admin/LoginController.java
@@ -5,12 +5,14 @@ import com.cy.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -27,8 +29,13 @@ public class LoginController {
     private UserService userService;
 
     @GetMapping
-    public String loginPage(Model model) {
+    public String loginPage(Model model, HttpServletRequest request) {
         model.addAttribute("img","");
+        HttpSession session1 = request.getSession();
+        Object user1 = session1.getAttribute("user");
+        if (!StringUtils.isEmpty(user1)) {
+            return "admin/index";
+        }
         return "admin/login";
     }
 
