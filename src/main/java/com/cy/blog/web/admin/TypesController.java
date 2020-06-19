edf10b72310c3828b6fef594db305ddae679ec2c
Allennicion <zhangwenj19970616@Gmail.com>
2020-06-19 21:14:02 +0800

    2020-04-19

--- src/main/java/com/cy/blog/web/admin/TypesController.java
+++ src/main/java/com/cy/blog/web/admin/TypesController.java
@@ -2,6 +2,7 @@ package com.cy.blog.web.admin;
 
 import com.cy.blog.po.Type;
 import com.cy.blog.service.TypesService;
+import com.cy.blog.vo.AjaxJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -52,41 +49,34 @@ public class TypesController {
     }
 
     @PostMapping("/inputTypes")
-    public String inputTypes(@Valid Type type, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    @ResponseBody
+    public AjaxJson inputTypes(@Valid Type type) {
         Type t = typesService.getTypeByName(type.getName());
         if (t != null){
-            bindingResult.rejectValue("name","nameError","类型名称不能重复");
+            return AjaxJson.fail("类型名称不能重复");
         }
-        //BindingResult获取验证返回得结果
-        if(bindingResult.hasErrors()) {
-            return "/admin/types-input";
-        }
+
         t = typesService.saveType(type);
         if(t == null) {
-            redirectAttributes.addFlashAttribute("message","操作失败");
+            return AjaxJson.fail("操作失败");
         }else {
-            redirectAttributes.addFlashAttribute("message","操作成功");
+            return AjaxJson.success("操作成功","");
         }
-        return "redirect:/admin/types";
     }
 
     @PostMapping("/inputTypes/{id}")
-    public String inputTypes(@Valid Type type, BindingResult bindingResult, @PathVariable Long id, RedirectAttributes redirectAttributes) {
+    @ResponseBody
+    public AjaxJson inputTypes(@Valid Type type, @PathVariable Long id) {
         Type t = typesService.getTypeByName(type.getName());
         if (t != null){
-            bindingResult.rejectValue("name","nameError","类型名称不能重复");
-        }
-        //BindingResult获取验证返回得结果
-        if(bindingResult.hasErrors()) {
-            return "/admin/types-input";
+            return AjaxJson.fail("类型名称不能重复");
         }
         t = typesService.updateType(id,type);
         if(t == null) {
-            redirectAttributes.addFlashAttribute("message","更新失败");
+            return AjaxJson.fail("更新失败");
         }else {
-            redirectAttributes.addFlashAttribute("message","更新成功");
+            return AjaxJson.success("操作成功","");
         }
-        return "redirect:/admin/types";
     }
 
     @RequestMapping("/types/{id}/delete")
